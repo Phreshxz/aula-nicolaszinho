1388627a2602ca488cc759d1594c77586dd9ec77
Phreshxz <amaral.souza.pedro@escola.pr.gov.br>
2025-10-23 17:09:35 -0300

    Add files via upload

---
+++ ATIVIDADE JAVA/script.js
@@ -0,0 +1,60 @@
+const x = 10
+
+const alunos = ["max", "japa", "satan"]
+
+
+
+console.log(alunos)
+
+function somar(n1, n2){
+    return n1 + n2
+}
+
+
+function bomdia(nome){
+
+    return "bomdia meu queido(a)" + nome
+}
+
+console.log(somar(10,5))
+console.log(somar(12,7))
+console.log(bomdia("MAX"))
+
+
+function sendText(){
+let texto = document.getElementById("texto").value
+alert
+
+
+
+}
+
+function somarNumeros(){
+
+let n1 = Number(document.getElementById("n1").value)
+let n2 = Number(document.getElementById("n2").value)
+alert(n1+n2)
+
+}
+function subNumeros(){
+
+let n1 = Number(document.getElementById("n1").value)
+let n2 = Number(document.getElementById("n2").value)
+alert(n1-n2)
+
+}
+function divNumeros(){
+
+let n1 = Number(document.getElementById("n1").value)
+let n2 = Number(document.getElementById("n2").value)
+alert(n1/n2)
+
+}
+function multNumeros(){
+
+let n1 = Number(document.getElementById("n1").value)
+let n2 = Number(document.getElementById("n2").value)
+alert(n1*n2)
+
+}
+
